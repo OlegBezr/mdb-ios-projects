b815c6f47037593666116372fe8ec1b6d646946f
Michael Lin <miclin@berkeley.edu>
2021-08-19 20:53:25 -0500

    Update comments in SceneDelegate

--- Meet the Members/Meet the Members/Support Files/SceneDelegate.swift
+++ Meet the Members/Meet the Members/Support Files/SceneDelegate.swift
@@ -18,13 +18,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         // MARK: STEP 4: Setup entry point
         // Action Items:
-        // - Read the code. You might not understand it, so here's what it does
-        //   and why we do it:
+        // - Read the code. Here's what it does and why we do it:
         //      Usually, the initial view controller is initialized by IB.
         //      But because we are not using IB for this project, we need to
         //      manually do the initialization in code. You can try to remove
-        //      the five lines and you will see a runtime error about entry point
-        //      not set.
+        //      the five lines in this method and you will see a runtime
+        //      error about entry point not set.
         
         //      This delegate method is called every time the App is launched.
         //      So we will use it to initialize a `StartVC` and assign it to the
@@ -37,35 +36,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window!.rootViewController = StartVC()
         window!.makeKeyAndVisible()
     }
+    
+    // More UIScene lifecycle delegate methods. Don't worry about them.
 
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
+    func sceneDidDisconnect(_ scene: UIScene) {}
 
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
+    func sceneDidBecomeActive(_ scene: UIScene) {}
 
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
+    func sceneWillResignActive(_ scene: UIScene) {}
 
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
+    func sceneWillEnterForeground(_ scene: UIScene) {}
 
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
+    func sceneDidEnterBackground(_ scene: UIScene) {}
 }
 
